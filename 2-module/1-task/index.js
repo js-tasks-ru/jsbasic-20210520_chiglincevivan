14053d6f260b2832429237fd47cc78cd44320bdd
chiglintsev-learn <84636098+chiglintsev-learn@users.noreply.github.com>
2021-05-30 13:01:39 +0300

    2-module 1-task: salary calc; probably bad code

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,15 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let salary = 0;
+
+  for (let _obj in salaries) {
+    if (isPositiveNumber(salaries[_obj])) salary += salaries[_obj]
+  }
+  return salary;
+}
+
+function isPositiveNumber(_num) {
+  if (_num == NaN) return false
+  if (_num == Infinity) return false
+  if (_num > 0) return true
+  else return false
 }
