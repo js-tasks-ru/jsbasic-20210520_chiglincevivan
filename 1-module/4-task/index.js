963f4668de35ab9a84a4c689595359b44973bdea
chiglintsev-learn <84636098+chiglintsev-learn@users.noreply.github.com>
2021-05-30 11:11:17 +0300

    1-modile 4-task what a mess I did :|

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,11 @@
+let blackList = ['XXX', '1xBet']
+
 function checkSpam(str) {
-  // ваш код...
+  for (i = 0; blackList[i]; i++) {
+    let blackListedKey = (blackList[i].toLowerCase());
+
+    if (str.toLowerCase().includes(blackListedKey)) return true
+    else continue;
+  }
+  return false;
 }
