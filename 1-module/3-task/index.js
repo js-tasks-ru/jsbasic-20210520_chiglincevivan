16d42787386d428fd91b73d8cdce40f77f110a2b
chiglintsev-learn <84636098+chiglintsev-learn@users.noreply.github.com>
2021-05-30 10:44:28 +0300

    1-module 3-task: caps 1-st letter if so

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,5 @@
 function ucFirst(str) {
-  // ваш код...
+  if (str.length == 0) return '';
+  if (str.length == 1) return str[0].toUpperCase();
+  if (str.length > 0 && str[0]) return str[0].toUpperCase() + str.slice(1);
 }
