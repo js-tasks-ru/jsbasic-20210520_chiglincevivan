654f92f0941d91c756a82f03d826c0a5a4ad99f2
chiglintsev-learn <84636098+chiglintsev-learn@users.noreply.github.com>
2021-05-30 13:43:25 +0300

    2-module 3-task: calc

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,19 @@
-let calculator = {
-  // ваш код
+const calculator = {
+  a: 0,
+  b: 0,
+
+  read(a, b) {
+    this.a = +a;
+    this.b = +b;
+  },
+  
+  sum(a, b) {
+    return this.a + this.b
+  },
+
+  mul(a, b){
+    return this.a * this.b
+  },
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
