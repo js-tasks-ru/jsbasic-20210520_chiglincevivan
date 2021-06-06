0141dd55791691f421b391030b39439b37e0d0ac
chiglintsev-learn <84636098+chiglintsev-learn@users.noreply.github.com>
2021-06-06 22:40:56 +0300

    3-module

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,3 +1,32 @@
+// function namify(users) {
+//   const arr = [];
+//   for (_obj of users) {
+//     arr.push(_obj['name']);
+//   }
+//   return arr
+// }
+
+
+// function namify(users) {
+//   const arr = [];
+//   users.forEach(_obj => arr.push(_obj['name']));
+//   return arr
+// }
+
+
+// function namify(users) {
+//   return users.map(user => user['name'])
+// }
+
 function namify(users) {
-  // ваш код...
+  return users.reduce((arr, _user) => arr.concat(_user['name']), [])
 }
+
+// for glory of test
+// let vasya = { name: 'Вася', age: 25 };
+// let petya = { name: 'Петя', age: 30 };
+// let masha = { name: 'Маша', age: 28 };
+
+// let users = [ vasya, petya, masha ];
+
+// console.log(namify(users))
