4254a471a8a0954f808939984c7cfdcd4ae6ae6d
chiglintsev-learn <84636098+chiglintsev-learn@users.noreply.github.com>
2021-06-07 12:05:49 +0300

    3-task

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,20 @@
 function camelize(str) {
-  // ваш код...
+  let arr = str.split('-');
+  // console.log(arr);
+  if (arr.length < 2) return str
+  for(i = 1; i < arr.length; i++) {
+    // console.log(arr[i]);
+    if (arr[i]) {
+      arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1);
+      // console.log(arr[i]);
+    }
+  }
+  return arr.join('')
 }
+
+
+// console.log(camelize('backgroundcolor'));
+// console.log(camelize('background-color'));
+// console.log(camelize('list-style-image'));
+// console.log(camelize('-webkit-transition'));
+// console.log(camelize('-we---------bkit--transition'));
