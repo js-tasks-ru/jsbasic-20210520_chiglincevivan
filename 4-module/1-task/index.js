e11282e76ca1f7bea52f6c75312cfde2f68878a3
chiglintsev-learn <84636098+chiglintsev-learn@users.noreply.github.com>
2021-06-08 20:12:25 +0300

    4-module 1-task

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,24 @@
+// todo:
+// сделать свойства объектов в строку
+// строки засунуть в li
+// все li засунуть в ul
+
 function makeFriendsList(friends) {
-  // ваш код...
+  // создаём <li> 
+  // внутрь помещаем созданную из объекта с помощью map
+  const strLi = friends.map(_obj => {
+    const _li = document.createElement('li');
+    _li.innerText = `${_obj['firstName']} ${_obj['lastName']}`;
+    return _li
+  });
+
+  // создаём ul
+  const ul = document.createElement('ul');
+
+  // помещаем все созданные li в ul
+  for (ele of strLi) {
+    ul.insertAdjacentElement('beforeend', ele);
+  }
+
+  return ul
 }
