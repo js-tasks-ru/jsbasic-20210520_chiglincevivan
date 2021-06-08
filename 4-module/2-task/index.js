64babd18969077ddd0fa6095f000557ca5ceadfa
chiglintsev-learn <84636098+chiglintsev-learn@users.noreply.github.com>
2021-06-08 21:22:29 +0300

    4-module 2-task

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,16 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  const y = table.rows.length; // количество строк
+
+  // высчитываем количество ячеек в строках
+  const _arr = [];
+  for (i = 0; i < y; i++){
+    _arr.push(table.rows[i].cells.length);
+  }
+  const x = Math.min(..._arr); // количество стоблцов
+
+  const minXY = Math.min(x, y); 
+  
+  for (i = 0; i < minXY; i++) {
+    table.rows[i].cells[i].style.backgroundColor = 'red';
+  }
 }
