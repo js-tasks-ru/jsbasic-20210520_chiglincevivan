3a5b5a556dd9c81f5109cb3e8692399b4b203d4f
chiglintsev-learn <84636098+chiglintsev-learn@users.noreply.github.com>
2021-06-17 18:23:58 +0300

    3-task

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,16 @@
 function highlight(table) {
-  // ваш код...
+  for (tr of table.tBodies[0].querySelectorAll('tr')) {
+    const status = tr.querySelector('td:nth-child(4)');
+    if (!status.hasAttribute("data-available")) tr.setAttribute('hidden', '');
+    if (status.dataset.available == 'true') tr.classList.add('available');
+    if (status.dataset.available == 'false') tr.classList.add('unavailable');
+
+    const age = tr.querySelector('td:nth-child(2)');
+    if (+age.textContent < 18) tr.setAttribute('style', 'text-decoration: line-through;')
+
+    const sex = tr.querySelector('td:nth-child(3)');
+    if (sex.textContent == 'm') tr.classList.add('male');
+    if (sex.textContent == 'f') tr.classList.add('female')
+
+  }
 }
