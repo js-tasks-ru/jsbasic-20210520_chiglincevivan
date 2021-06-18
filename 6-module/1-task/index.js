5229a0c0c24ae1151522d8289a681880ca0e22ea
chiglintsev-learn <84636098+chiglintsev-learn@users.noreply.github.com>
2021-06-18 21:34:55 +0300

    adding extra learn folder into ignore

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,36 @@
  */
 export default class UserTable {
   constructor(rows) {
+
   }
 }
+
+let rows = [
+  {
+      name: 'Вася',
+      age: 25,
+      salary: 1000,
+      city: 'Самара'
+  },
+  {
+      name: 'Петя',
+      age: 30,
+      salary: 1500,
+      city: 'Москва'
+  }
+];
+
+// table 
+// tr
+// td name
+// td age
+// td salary
+// td city
+// td button
+
+
+const newMap = rows
+  .map(element => `<tr><td>${element.name}</td><td>${element.age}</td><td>${element.salary}</td><td>${element.city}</td><td><button>X</button></td></tr>`).join('');
+
+
+document.body.insertAdjacentHTML('beforeend', `<table>${newMap}</table>`);
